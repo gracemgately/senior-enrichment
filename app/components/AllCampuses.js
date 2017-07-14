cfabd138aa602ed96b74377b0b527cfef10ca40c
Grace Gately <gracemgately@gmail.com>
2017-07-14 14:33:07 -0400

    added campus images

--- app/components/AllCampuses.js
+++ app/components/AllCampuses.js
@@ -31,7 +31,9 @@ export default class AllCampuses extends Component {
                     {this.state.campuses.map(campus => {
                         return (
                             <div className='row' key={campus.id}>
-                            <li><Link to={`/campuses/${campus.id}`}>{campus.name}</Link></li>
+                            <img src={'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Africa_and_Europe_from_a_Million_Miles_Away_%28cropped%29.png/100px-Africa_and_Europe_from_a_Million_Miles_Away_%28cropped%29.png'}/>
+                            <li>
+                            <Link to={`/campuses/${campus.id}`}>{campus.name}</Link></li>
                                 <Route path={`/campuses/${campus.id}`} render={() => (
                                     <SingleCampus singleCampus={campus.id}/>)} />
                             </div>
